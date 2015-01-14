9e608ac5109e2d660cbb390793324d58f9fdceac
Jakub Mysiak <jakub@jakubmysiak.com>
2015-01-13 23:43:55 -0600

    Style lateral nav and links

--- js/main.js
+++ js/main.js
@@ -13,7 +13,20 @@ jQuery(document).ready(function($){
       // firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
       $('body').toggleClass('overflow-hidden');
     });
+
     $('#lateral-nav').toggleClass('lateral-menu-is-open');
+
+    // if( $('.main-content').css('transform') != 'none') {
+    //   if( !$('.main-content').hasClass('lateral-menu-is-open')) {
+    //     $('.main-content').css('transform', 'translateX(0px)');
+    //   }
+    //   else {
+    //     $('.main-content').css('transform', 'translateX(260px)');
+    //   }
+    // }
+
+    $('.slideshow').toggleClass('slideshow-active');
+
     //check if submenu is open
     if($('#workLink').hasClass('submenu-open')) {
       $('#workLink').removeClass('submenu-open').next('.sub-menu').delay(1200).slideUp(200);
@@ -24,29 +37,37 @@ jQuery(document).ready(function($){
     }
   });
 
+
   //close lateral menu clicking outside the menu itself
-  $content_wrapper.on('click', function(event){
-    if( !$(event.target).is('#menu-trigger, #menu-trigger span') ) {
-      $lateral_menu_trigger.removeClass('is-clicked');
-      $navigation.removeClass('lateral-menu-is-open');
-      $content_wrapper.removeClass('lateral-menu-is-open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-        $('body').removeClass('overflow-hidden');
-      });
-      $('#lateral-nav').removeClass('lateral-menu-is-open');
-      //check if submenu is open
-      if($('#workLink').hasClass('submenu-open')) {
-        $('#workLink').removeClass('submenu-open').next('.sub-menu').delay(1200).slideUp(200);
-      }
-      //check if transitions are not supported
-      if($('html').hasClass('no-csstransitions')) {
-        $('body').removeClass('overflow-hidden');
-      }
+  // $content_wrapper.on('click', function(event){
+  //   if( !$(event.target).is('#menu-trigger, #menu-trigger span') ) {
+  //     $lateral_menu_trigger.removeClass('is-clicked');
+  //     $navigation.removeClass('lateral-menu-is-open');
+  //     $content_wrapper.removeClass('lateral-menu-is-open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+  //       $('body').removeClass('overflow-hidden');
+  //     });
+  //     $('#lateral-nav').removeClass('lateral-menu-is-open');
+  //     //check if submenu is open
+  //     if($('#workLink').hasClass('submenu-open')) {
+  //       $('#workLink').removeClass('submenu-open').next('.sub-menu').delay(1200).slideUp(200);
+  //     }
+  //     //check if transitions are not supported
+  //     if($('html').hasClass('no-csstransitions')) {
+  //       $('body').removeClass('overflow-hidden');
+  //     }
+  //   }
+  // });
+
+  //open (or close) submenu items in the lateral menu. Close all the other open submenu items.
+  $('#workLink').on('click', function(event){
+    event.preventDefault();
+    $(this).toggleClass('submenu-open').next('.sub-menu').slideToggle('300', 'easeInOutCubic');
+  });
+
+  $('.item-no-children').children('a').on('click', function(event) {
+    if( $('#workLink').hasClass('submenu-open')) {
+      $('#workLink').toggleClass('submenu-open').next('.sub-menu').slideToggle('300', 'easeInOutCubic');
     }
   });
 
-  //open (or close) submenu items in the lateral menu. Close all the other open submenu items.
-  $('.item-has-children').children('a').on('click', function(event){
-    event.preventDefault();
-    $(this).toggleClass('submenu-open').next('.sub-menu').slideToggle(200).end().parent('.item-has-children').siblings('.item-has-children').children('a').removeClass('submenu-open').next('.sub-menu').slideUp(200);
-  });
 });
